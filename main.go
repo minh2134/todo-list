9d18f321499032a062ad442c062048e423c191cb
Minh Pham <minh21345@gmail.com>
2026-07-05 04:01:02 +0700

    added functions and routes to implement later

--- main.go
+++ main.go
@@ -7,9 +7,30 @@ func main() {
 
 	mux.Handle("GET /", http.FileServer(http.Dir("./static")))
 	mux.HandleFunc("GET /clicked", clicked)
+
+	mux.HandleFunc("GET /task", list)
+	mux.HandleFunc("POST /task", add)
+	mux.HandleFunc("DELETE /task", del)
+	mux.HandleFunc("PATCH /task", edit)
 	http.ListenAndServe(":3000", mux)
 }
 
 func clicked(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("You clicked!"))
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	// TODO: handle query parameters to filter the result
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	// TODO: add the posted task
+}
+
+func del(w http.ResponseWriter, r *http.Request) {
+	// TODO: delete the task (handle path and query params)
+}
+
+func edit(w http.ResponseWriter, r *http.Request) {
+	// TODO: edit the task's data based on body (handle path)
 }
