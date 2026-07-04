5e96337d6b13a666f56f6ae65c253b2407a939ab
Minh Pham <minh21345@gmail.com>
2026-07-05 03:41:29 +0700

    htmx basics

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "net/http"
+
 func main() {
-	println("Hello world")
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /", http.FileServer(http.Dir("./static")))
+	mux.HandleFunc("GET /clicked", clicked)
+	http.ListenAndServe(":3000", mux)
+}
+
+func clicked(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("You clicked!"))
 }
