5d520849b4c06f16ea538f6ab480c7b8e6174adf
Minh Pham <minh21345@gmail.com>
2026-07-07 04:25:42 +0700

    Add an add a task dialog

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"todo-list/internal/task"
@@ -18,17 +19,12 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /", http.FileServer(http.Dir("./static")))
-	mux.HandleFunc("GET /clicked", clicked)
 
 	mux.HandleFunc("GET /task", list)
 	mux.HandleFunc("POST /task", add)
 	mux.HandleFunc("DELETE /task", del)
 	mux.HandleFunc("PATCH /task", edit)
 	http.ListenAndServe(":3000", mux)
-}
-
-func clicked(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("You clicked!"))
 }
 
 func list(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +35,8 @@ func list(w http.ResponseWriter, r *http.Request) {
 
 func add(w http.ResponseWriter, r *http.Request) {
 	// TODO: add the posted task
+	fmt.Println(r.PostFormValue("name"))
+	fmt.Println(r.PostFormValue("desc"))
 }
 
 func del(w http.ResponseWriter, r *http.Request) {
